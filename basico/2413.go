a8683d5a078dd4870d381228042ca2e8a3fcf5aa
root <root@DESKTOP-KLAMNUL>
2023-10-23 13:23:48 -0300

    adding 2413

---
+++ basico/2413.go
@@ -0,0 +1,13 @@
+package basico
+
+import "fmt"
+
+func Bee2313() {
+	var Tpessoas int
+
+    fmt.Scanf("%d", &Tpessoas)
+    
+    Tpessoas = Tpessoas * 4
+    
+    fmt.Printf("%d\n", Tpessoas)
+}
